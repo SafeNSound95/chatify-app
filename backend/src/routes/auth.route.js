6e797f4d01700e691d6c9adcfe170941be9aad30
Saif Shalan <safensound95@gmail.com>
2026-04-14 14:35:40 +0300

    Setup mongodb connection, define user schema, implement signup controller with jwt and bcrypt.

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup endpoint");
-});
+import { signup } from "../controllers/auth.controller.js";
+
+router.post("/signup", signup);
 
 router.get("/login", (req, res) => {
   res.send("Login endpoint");
