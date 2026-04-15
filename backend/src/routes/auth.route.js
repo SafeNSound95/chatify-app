329ad66469728666270a8429f80bc543d2f08c4c
Saif Shalan <safensound95@gmail.com>
2026-04-15 12:35:55 +0300

    feat: implement user login and logout

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 router.post("/signup", signup);
 
-router.get("/login", (req, res) => {
-  res.send("Login endpoint");
-});
+router.post("/login", login);
 
-router.get("/logout", (req, res) => {
-  res.send("Logout endpoint");
-});
+router.post("/logout", logout);
 
 export default router;
