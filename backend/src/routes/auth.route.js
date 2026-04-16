570c514e22eeb980c23dfdc44da0b07413a6f925
Saif Shalan <safensound95@gmail.com>
2026-04-16 15:53:43 +0300

    feat: implement rate limiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -8,8 +8,11 @@ import {
 } from "../controllers/auth.controller.js";
 
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 
